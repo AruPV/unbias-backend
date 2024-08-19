299f44c48130b32a8710e1f9a11c4bfc8f91adf1
Aru Poleo Vargas <arupoleo@gmail.com>
2024-08-19 12:53:07 -0400

    make app less secure to solve cors hopefully

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,10 +8,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   Rails.logger.info "FRONTEND_URL: #{ENV['FRONTEND_URL']}"
   allow do
-    origins ENV["FRONTEND_URL"]
+    origins "*"
 
     resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
   end
 end
