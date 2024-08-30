5d0111e2502b0b45f53c76190c85580877855083
Aru Poleo Vargas <arupoleo@gmail.com>
2024-08-30 12:09:47 -0400

    added endpoint for clerk webhook

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 
   # POST /users
   def create
-    @user = User.new(user_params)
+    @user = User.new(clerk_id: user_params["id"])
 
     if @user.save
       render json: @user, status: :created, location: @user
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :email)
+      params.require(:data).permit(:id)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :articles
   has_many :votes, dependent: :destroy
+  validates :clerk_id, presence: true, uniqueness: true
 end
